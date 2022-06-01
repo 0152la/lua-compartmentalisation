c47f5087217aab81671b555355fbc1fdd8be7a3c
Andrei Lascu <andrei.lascu@kcl.ac.uk>
2022-06-01 13:08:51 +0100

    Cleanup

--- tests/simple.c
+++ tests/simple.c
@@ -1,12 +1,6 @@
 #include <stdio.h>
 #include <sys/time.h>
 
-/*int __real___vdso_clock_gettime(clockid_t clock_id, struct timespec* ts);*/
-/*int __wrap___vdso_clock_gettime(clockid_t clock_id, struct timespec* ts)*/
-/*{*/
-    /*return __real___vdso_clock_gettime(clock_id, ts);*/
-/*}*/
-
 int
 main(void)
 {
